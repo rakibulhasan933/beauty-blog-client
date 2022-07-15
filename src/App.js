f1feaddb922a8db92835f38593a425065af83af5
RAKIBUL <rakibulmd933@gmail.com>
2022-07-15 22:30:48 +0600

    header part

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import TopBer from './components/TopBer/TopBer';
+import Home from './Pages/Home/Home';
 
 
 const App = () => {
     return (
         <div>
             <TopBer />
+            <Home />
         </div>
     );
 };
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../components/Header/Header';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div className='home'>
+            <Header />
+        </div>
+    );
+};
+
+export default Home;
