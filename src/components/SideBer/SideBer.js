e124eff6a8edd0d9198c383fa54952d5a843a9b4
RAKIBUL <rakibulmd933@gmail.com>
2022-07-16 17:24:51 +0600

    sidebar menu

--- src/components/SideBer/SideBer.js
+++ src/components/SideBer/SideBer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './SideBer.css';
 
 const SideBer = () => {
@@ -14,6 +15,50 @@ const SideBer = () => {
                     Laboris sunt aute cupidatat velit magna velit ullamco dolore mollit
                     amet ex esse.Sunt eu ut nostrud id quis proident.
                 </p>
+            </div>
+            <div className="sidebarItem">
+                <span className="sidebarTitle">CATEGORIES</span>
+                <ul className="sidebarList">
+                    <li className="sidebarListItem">
+                        <Link className="link" to="/">
+                            Life
+                        </Link>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Link className="link" to="/">
+                            Music
+                        </Link>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Link className="link" to="/">
+                            Sport
+                        </Link>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Link className="link" to="/">
+                            Style
+                        </Link>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Link className="link" to="/">
+                            Tech
+                        </Link>
+                    </li>
+                    <li className="sidebarListItem">
+                        <Link className="link" to="/">
+                            Cinema
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="sidebarItem">
+                <span className="sidebarTitle">FOLLOW US</span>
+                <div className="sidebarSocial">
+                    <i className="sidebarIcon fab fa-facebook-square"></i>
+                    <i className="sidebarIcon fab fa-instagram-square"></i>
+                    <i className="sidebarIcon fab fa-pinterest-square"></i>
+                    <i className="sidebarIcon fab fa-twitter-square"></i>
+                </div>
             </div>
         </div>
     );
