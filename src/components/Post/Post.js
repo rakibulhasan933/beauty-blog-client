a0fc970a8e193d2cab6d9d2d56fda0ed06ac63f1
RAKIBUL <rakibulmd933@gmail.com>
2022-07-17 00:48:19 +0600

    post created

---
+++ src/components/Post/Post.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Post.css';
+
+const Post = () => {
+    return (
+        <div className='post'>
+            <img className='postImg' src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
+            <div className="postInfo">
+                <div className="postCats">
+                    <span className="postCat">
+                        <Link className="link" to="/">
+                            Music
+                        </Link>
+                    </span>
+                    <span className="postCat">
+                        <Link className="link" to="/">
+                            Life
+                        </Link>
+                    </span>
+                </div>
+                <span className="postTitle">
+                    <Link to="/" className="link">
+                        Lorem ipsum dolor sit amet
+                    </Link>
+                </span>
+                <hr />
+                <span className="postDate">1 hour ago</span>
+            </div>
+            <p className="postDesc">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
+                officia architecto deserunt deleniti? Labore ipsum aspernatur magnam
+                fugiat, reprehenderit praesentium blanditiis quos cupiditate ratione
+                atque, exercitationem quibusdam, reiciendis odio laboriosam?
+            </p>
+        </div>
+    );
+};
+
+export default Post;
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Post from '../Post/Post';
 import './Posts.css';
 
 const Posts = () => {
     return (
         <div className='posts'>
-            Post
+            <Post />
         </div>
     );
 };
