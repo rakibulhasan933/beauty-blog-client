13c4c262db766d1b0a2775e64602235268b02b1e
RAKIBUL <rakibulmd933@gmail.com>
2022-07-17 22:32:44 +0600

    topbar

--- src/components/TopBer/TopBer.js
+++ src/components/TopBer/TopBer.js
@@ -35,7 +35,7 @@ const TopBer = () => {
             </div>
             <div className="topRight">
                 <ul className="topList">
-                    <Link className="link" to="/">
+                    <Link className="link" to="/settings">
                         <img
                             className="topImg"
                             src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
