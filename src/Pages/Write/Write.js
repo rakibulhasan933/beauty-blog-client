33a207afb723ff4d0d3cd2ccfcefd38488dd2f1b
RAKIBUL <rakibulmd933@gmail.com>
2022-07-17 10:04:46 +0600

    from create

--- src/Pages/Write/Write.js
+++ src/Pages/Write/Write.js
@@ -3,8 +3,37 @@ import './Write.css';
 
 const Write = () => {
     return (
-        <div>
-            Write
+        <div className='write'>
+            <img
+                className="writeImg"
+                src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                alt=""
+            />
+            <from className="writeForm">
+                <div className="writeFromGroup">
+                    <label htmlFor="fileInput">
+                        <i className="writeIcon fas fa-plus"></i>
+                    </label>
+                    <input id="fileInput" type="file" style={{ display: "none" }} />
+                    <input
+                        className="writeInput"
+                        placeholder="Title"
+                        type="text"
+                        autoFocus={true}
+                    />
+                </div>
+                <div className="writeFormGroup">
+                    <textarea
+                        className="writeInput writeText"
+                        placeholder="Tell your story..."
+                        type="text"
+                        autoFocus={true}
+                    />
+                </div>
+                <button className="writeSubmit" type="submit">
+                    Publish
+                </button>
+            </from>
         </div>
     );
 };
