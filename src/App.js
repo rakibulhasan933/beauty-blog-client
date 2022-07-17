1f89e65c18a60a3f0b352e0621f4595a3caff7bb
RAKIBUL <rakibulmd933@gmail.com>
2022-07-17 15:49:19 +0600

    setting title

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import './App.css';
 import TopBer from './components/TopBer/TopBer';
-import Settings from './Pages/Settings/Settings';
-import Write from './Pages/Write/Write';
 
 
 const App = () => {
     return (
         <div>
             <TopBer />
-            <Settings />
+
         </div>
     );
 };
