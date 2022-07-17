1a1ed8a1679dc4737da93087bfd264276042a19d
RAKIBUL <rakibulmd933@gmail.com>
2022-07-17 09:59:57 +0600

    write add

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import TopBer from './components/TopBer/TopBer';
 import Single from './Pages/Single/Single';
+import Write from './Pages/Write/Write';
 
 
 const App = () => {
     return (
         <div>
             <TopBer />
-            <Single />
+            <Write />
         </div>
     );
 };
---
+++ src/Pages/Write/Write.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Write.css';
+
+const Write = () => {
+    return (
+        <div>
+            Write
+        </div>
+    );
+};
+
+export default Write;
