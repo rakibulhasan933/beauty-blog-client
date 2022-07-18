0e10fb620c53b54ee7cba896c7ddc9bcd22ee335
RAKIBUL <rakibulmd933@gmail.com>
2022-07-19 00:13:11 +0600

    footer add

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <hr />
+            <p>Copyright © 2022 RedQ, Inc.</p>
+        </div>
+    );
+};
+
+export default Footer;
