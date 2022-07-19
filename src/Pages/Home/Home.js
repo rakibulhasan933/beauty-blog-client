2e162e83735f7b9b8956b489d6f248e622655ae1
RAKIBUL <rakibulmd933@gmail.com>
2022-07-19 13:09:35 +0600

    post

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../../components/Header/Header';
 import Posts from '../Posts/Posts';
 import './Home.css';
@@ -7,7 +8,9 @@ const Home = () => {
     return (
         <div>
             <Header />
-            <Posts />
+            <Link className="link" to='/blogs'>
+                <Posts />
+            </Link>
         </div>
     );
 };
