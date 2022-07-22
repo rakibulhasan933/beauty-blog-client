ba286ecdda0d4fc91b183072d56e8ef134f9ac33
RAKIBUL <rakibulmd933@gmail.com>
2022-07-22 12:24:32 +0600

    single blogs

--- src/Pages/SinglePost/SinglePost.js
+++ src/Pages/SinglePost/SinglePost.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import './SinglePost.css';
 
 const SinglePost = () => {
     const { id } = useParams();
-    console.log(id);
+    const [blogs, setBlogs] = useState({});
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/blogs/${id}`)
+            .then(res => res.json())
+            .then(data => setBlogs(data))
+    }, [id]);
+
+    const { name, tittle, time, image, dec } = blogs;
     return (
         <div className='singlePost'>
             <div className="singlePostWrapper">
                 <img
                     className="singlePostImg"
-                    src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                    src={`data:image/jpg;base64,${image}`}
                     alt=""
                 />
                 <h1 className="singlePostTitle">
-                    Lorem ipsum dolor
+                    {tittle}
                     <div className="singlePostEdit">
                         <i className="singlePostIcon far fa-edit"></i>
                         <i className="singlePostIcon far fa-trash-alt"></i>
@@ -24,40 +32,13 @@ const SinglePost = () => {
                     <span>
                         Author:
                         <b className="singlePostAuthor">
-                            Rakibul
+                            {name}
                         </b>
                     </span>
-                    <span>1 day ago</span>
+                    <span>{time}day ago</span>
                 </div>
                 <p className="singlePostDesc">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste error
-                    quibusdam ipsa quis quidem doloribus eos, dolore ea iusto impedit!
-                    Voluptatum necessitatibus eum beatae, adipisci voluptas a odit modi
-                    eos! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste
-                    error quibusdam ipsa quis quidem doloribus eos, dolore ea iusto
-                    impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas a
-                    odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
-                    elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
-                    iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
-                    a odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
-                    elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
-                    iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
-                    a odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
-                    elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
-                    iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
-                    a odit modi eos!
-                    <br />
-                    <br />
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste error
-                    quibusdam ipsa quis quidem doloribus eos, dolore ea iusto impedit!
-                    Voluptatum necessitatibus eum beatae, adipisci voluptas a odit modi
-                    eos! Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste
-                    error quibusdam ipsa quis quidem doloribus eos, dolore ea iusto
-                    impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas a
-                    odit modi eos! Lorem, ipsum dolor sit amet consectetur adipisicing
-                    elit. Iste error quibusdam ipsa quis quidem doloribus eos, dolore ea
-                    iusto impedit! Voluptatum necessitatibus eum beatae, adipisci voluptas
-                    a odit modi eos! Lorem, ipsum dolor sit amet consectetur.
+                    {dec}
                 </p>
             </div>
         </div>
