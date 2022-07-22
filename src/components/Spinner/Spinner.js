2b255bd28b3810932ea93370b8dc52a881bcf1d0
RAKIBUL <rakibulmd933@gmail.com>
2022-07-22 19:21:43 +0600

    spinner

---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Spinner.css'
+
+const Spinner = () => {
+    return (
+        <div>
+            <h1 className='load'>Loading...</h1>
+        </div>
+    );
+};
+
+export default Spinner;
