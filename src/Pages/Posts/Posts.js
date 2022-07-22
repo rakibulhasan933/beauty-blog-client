3ff8e0ee051afb80c0315073d16f34d88b04d82c
RAKIBUL <rakibulmd933@gmail.com>
2022-07-22 19:42:50 +0600

    loacl data removed

--- src/Pages/Posts/Posts.js
+++ src/Pages/Posts/Posts.js
@@ -2,50 +2,6 @@ import React, { useEffect, useState } from 'react';
 import Post from '../Post/Post';
 import './Posts.css';
 import Spinner from '../../components/Spinner/Spinner';
-const data = [
-    {
-        _id: 0,
-        name: "Learning Java",
-        dec: " loremLorem ipsum dolor sit am loremLorem ipsum dolor sit amloremLorem ipsum dolor sit amloremLorem ipsum dolor sit amvv",
-        time: "1 day ago",
-        img: "https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-    },
-    {
-        _id: 1,
-        name: "Learning Meshine ",
-        dec: " loremLorem ipsum dolor sit am loremLorem ipsum dolor sit amloremLorem ipsum dolor sit amloremLorem ipsum dolor sit amvv",
-        time: "6 hour ago",
-        img: "https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-    },
-    {
-        _id: 2,
-        name: "Learning React",
-        dec: " loremLorem ipsum dolor sit am loremLorem ipsum dolor sit amloremLorem ipsum dolor sit amloremLorem ipsum dolor sit amvv",
-        time: "4 hour ago",
-        img: "https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-    },
-    {
-        _id: 3,
-        name: "Learning CSS",
-        dec: " loremLorem ipsum dolor sit am loremLorem ipsum dolor sit amloremLorem ipsum dolor sit amloremLorem ipsum dolor sit amvv",
-        time: "3 hour ago",
-        img: "https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-    },
-    {
-        _id: 4,
-        name: "Learning React",
-        dec: " loremLorem ipsum dolor sit am loremLorem ipsum dolor sit amloremLorem ipsum dolor sit amloremLorem ipsum dolor sit amvv",
-        time: "2 hour ago",
-        img: "https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-    },
-    {
-        _id: 5,
-        name: "Learning C++",
-        dec: " loremLorem ipsum dolor sit am loremLorem ipsum dolor sit amloremLorem ipsum dolor sit amloremLorem ipsum dolor sit amvv",
-        time: "1 hour ago",
-        img: "https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-    }
-];
 
 const Posts = () => {
     const [blogs, setBlogs] = useState([]);
