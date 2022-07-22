f1ba79138ee86c1d2bf0dba827f6c64487b01610
RAKIBUL <rakibulmd933@gmail.com>
2022-07-22 21:37:54 +0600

    home

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import TopBer from './components/TopBer/TopBer';
-import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
 import Settings from './Pages/Settings/Settings';
 import Write from './Pages/Write/Write';
@@ -11,6 +10,7 @@ import Footer from './components/Footer/Footer';
 import NotFound from './Pages/NotFound/NotFound';
 import Posts from './Pages/Posts/Posts';
 import BlogMain from './Pages/BlogMain/BlogMain';
+import HomeMain from './Pages/BlogMain/BlogMain';
 
 
 
@@ -19,7 +19,7 @@ const App = () => {
         <div>
             <TopBer />
             <Routes>
-                <Route path='/' element={<Home />} />
+                <Route path='/' element={<HomeMain />} />
                 <Route path='login' element={<Login />} />
                 <Route path='/blogs' element={<BlogMain />}>
                     <Route index element={<Posts />} />
