a47353488a6e4c66aa7b02ee3f9759f40140e610
RAKIBUL <rakibulmd933@gmail.com>
2022-07-22 21:28:29 +0600

    home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 import Header from '../../components/Header/Header';
-import Posts from '../Posts/Posts';
+import SideBer from '../../components/SideBer/SideBer';
+import Spinner from '../../components/Spinner/Spinner';
+import HomeBlogs from '../HomeBlogs/HomeBlogs';
 import './Home.css';
 
 const Home = () => {
-    return (
+    const [isLoading, SetLoading] = useState(false);
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+        SetLoading(true);
+        fetch('http://localhost:5000/blogs')
+            .then(res => res.json())
+            .then(data => {
+                setBlogs(data.slice(0, 6));
+                SetLoading(false);
+            });
+    }, [])
+
+    return isLoading ? <Spinner /> : (
         <div>
             <Header />
             <div className="home">
                 <div className="blogs">
-
+                    {
+                        blogs.map(item => <HomeBlogs key={item._id} item={item} />)
+                    }
                 </div>
+                <SideBer />
             </div>
         </div>
     );
