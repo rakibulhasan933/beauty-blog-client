0a860f8fd9eb899748186330f25322de885c0a8d
RAKIBUL <rakibulmd933@gmail.com>
2022-07-23 10:14:52 +0600

    from

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import SideBer from '../../components/SideBer/SideBer';
 import './Contact.css';
 
 const Contact = () => {
     return (
-        <div>
+        <div className='contact'>
+            <div className="contactWrapper">
+                <h3>Contact Us</h3>
 
+            </div>
+            <SideBer />
         </div>
     );
 };
