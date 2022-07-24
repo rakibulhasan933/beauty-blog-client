b40e6ea76baf3816d8e74e38f0b1647021035132
RAKIBUL <rakibulmd933@gmail.com>
2022-07-25 00:30:09 +0600

    login page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import './Login.css';
 import GoogleButton from 'react-google-button';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
-import auth from '../../firebase.init';
+import useAuth from '../../hooks/useAuth';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 
 
 const Login = () => {
-    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const { singInUsingInGoogle, setUser } = useAuth() || {};
+    const location = useLocation();
+    const navigate = useNavigate();
 
+    const from = location?.state?.from?.pathname || '/';
+
+    const handleGoogleLogin = () => {
+        singInUsingInGoogle()
+            .then((result) => {
+                setUser(result.user);
+                navigate(from, { replace: true })
+            })
+    };
     return (
         <div className='login'>
             <span className="loginTitle">Login in <span className='google'>Google</span></span>
             <GoogleButton
-                onClick={() => signInWithGoogle()}
+                onClick={handleGoogleLogin}
             />
         </div>
     );
