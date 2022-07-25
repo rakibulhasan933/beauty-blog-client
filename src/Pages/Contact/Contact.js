4cf0e87b563c6d8a0ed51b8f75d81ff7a1882b37
RAKIBUL <rakibulmd933@gmail.com>
2022-07-26 01:10:23 +0600

    contact page

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -7,7 +7,18 @@ const Contact = () => {
         <div className='contact'>
             <div className="contactWrapper">
                 <h3>Contact Us</h3>
+                <div className="container">
+                    <form>
+                        <label for="fname">Name</label>
+                        <input type="text" id="fname" name="firstname" placeholder="Your name.." />
+                        <label for="lname">Email</label>
+                        <input type="text" id="lname" name="lastname" placeholder="Your last name.." />
+                        <label for="subject">Feedback</label>
+                        <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
 
+                        <input type="submit" value="Submit" />
+                    </form>
+                </div>
             </div>
             <SideBer />
         </div>
