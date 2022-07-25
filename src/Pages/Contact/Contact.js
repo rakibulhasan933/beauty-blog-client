1b216a2b551e51caf367484b9da92e94d228858c
RAKIBUL <rakibulmd933@gmail.com>
2022-07-26 02:09:57 +0600

    email

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import SideBer from '../../components/SideBer/SideBer';
+import emailjs from '@emailjs/browser';
 import './Contact.css';
 
 const Contact = () => {
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_v1wubeb', 'template_z1oksff', form.current, 'nwObZR03jMMFUK0AS')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+    }
     return (
         <div className='contact'>
             <div className="contactWrapper">
                 <div className="container">
                     <h3 className='contact-title label'>Contact Us</h3>
-                    <form>
+                    <form ref={form} onSubmit={sendEmail}>
                         <label for="name" className='label'>Name</label>
                         <input type="text" name="name" placeholder="Your name.." />
                         <label for="email" className='label'>Email</label>
@@ -16,7 +28,7 @@ const Contact = () => {
                         <label for="feedback" className='label'>Feedback</label>
                         <textarea id="feedback" name="feedback" placeholder="Write something.."></textarea>
 
-                        <input type="submit" value="Submit" />
+                        <input type="submit" value="Send" />
                     </form>
                 </div>
             </div>
