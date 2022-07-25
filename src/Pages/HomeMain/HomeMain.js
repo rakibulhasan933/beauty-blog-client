1b80023edf10317b00090fc8843889e351bbfe82
RAKIBUL <rakibulmd933@gmail.com>
2022-07-26 01:36:57 +0600

    contact home added

--- src/Pages/HomeMain/HomeMain.js
+++ src/Pages/HomeMain/HomeMain.js
@@ -12,6 +12,19 @@ const HomeMain = () => {
                 <Home />
                 <SideBer />
             </div>
+            <div className="container">
+                <h3 className='contact-title label'>Contact Us</h3>
+                <form>
+                    <label for="name" className='label'>Name</label>
+                    <input type="text" name="name" placeholder="Your name.." />
+                    <label for="email" className='label'>Email</label>
+                    <input type="email" name="email" placeholder="Your Email.." />
+                    <label for="feedback" className='label'>Feedback</label>
+                    <textarea id="feedback" name="feedback" placeholder="Write something.."></textarea>
+
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
         </div>
     );
 };
