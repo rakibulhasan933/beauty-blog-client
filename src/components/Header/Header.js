efeb9998bd14f2e40583ae497515637418a65e13
RAKIBUL <rakibulmd933@gmail.com>
2022-07-26 14:53:23 +0600

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,13 +5,13 @@ const Header = () => {
     return (
         <div className='header'>
             <div className="headerTitles">
-                <span className="headerTitleSm">React & Node</span>
-                <span className="headerTitleLg">BLOG</span>
+                <span className="headerTitleSm">Hacking & Technology Learning Tips</span>
+                <span className="headerTitleLg">BLOGS</span>
             </div>
             <img
                 className="headerImg"
-                src="https://images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
-                alt=""
+                src="https://i.ibb.co/LJQ4ys4/pexels-tima-miroshnichenko-5380589.jpg"
+                alt="code"
             />
         </div>
     );
