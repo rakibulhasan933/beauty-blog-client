ef13c1a2b600691c8f9c9a6356fd34b759c17137
RAKIBUL <rakibulmd933@gmail.com>
2022-07-26 13:55:41 +0600

    hook from added

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,34 +1,30 @@
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 import SideBer from '../../components/SideBer/SideBer';
 import emailjs from '@emailjs/browser';
 import './Contact.css';
+import { useForm } from 'react-hook-form';
 
 const Contact = () => {
-    const form = useRef();
-    const sendEmail = (e) => {
-        e.preventDefault();
+    const { register, formState: { errors } } = useForm();
+    const [emailValidation, setEmailValidation] = useState(true);
+    const [msgValidation, setmsgValidation] = useState(true);
 
-        emailjs.sendForm('service_v1wubeb', 'template_z1oksff', form.current, 'nwObZR03jMMFUK0AS')
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            });
-    }
     return (
         <div className='contact'>
             <div className="contactWrapper">
                 <div className="container">
                     <h3 className='contact-title label'>Contact Us</h3>
-                    <form ref={form} onSubmit={sendEmail}>
+                    <form onSubmit={sendEmail}>
                         <label for="name" className='label'>Name</label>
-                        <input type="text" name="name" placeholder="Your name.." />
+                        <input className='form-control' {...register("name", { required: true })} />
+                        {errors.name && <span>Name is required</span>}<br />
                         <label for="email" className='label'>Email</label>
-                        <input type="email" name="email" placeholder="Your Email.." />
+                        <input className='form-control' {...register("email", { required: true })} />
+                        {emailValidation ? '' : <span style={{ color: 'red' }}>Email is required</span>}<br />
                         <label for="feedback" className='label'>Feedback</label>
-                        <textarea id="feedback" name="feedback" placeholder="Write something.."></textarea>
-
-                        <input type="submit" value="Send" />
+                        <textarea className='form-control' {...register("message", { required: true })} />
+                        {msgValidation ? '' : <span style={{ color: 'red' }}>This fiels is required</span>}<br />
+                        <input type="submit" value="Send Message" />
                     </form>
                 </div>
             </div>
